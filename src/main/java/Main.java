8bd6da6fd8d9891fda43400b76d1505adf67f395
Grisha <grisha5991@gmail.com>
2019-10-06 23:05:27 +0300

    fixed #2 Вынес логику расчёта в отделыный класс

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        double distance = 7.5;
-        int landingAmount = 60;
-        int costOneKilometer = 20;
-        double cost;
-        double discount = 0;
-        int minimumDiscountAmount = 1000;
-        int maximumDiscount = 100;
-        int discountPercentage = 5;
-        int maximumDiscountPercentage = 100;
-
-        cost = landingAmount + distance * costOneKilometer;
-        if (cost > minimumDiscountAmount) {
-            discount = cost * discountPercentage / maximumDiscountPercentage;
-        }
-        if (discount <= maximumDiscount) {
-            cost -= discount;
-        }
-        if (discount > maximumDiscount) {
-            cost -= maximumDiscount;
-        }
+        taxiCalculation taxi = new taxiCalculation();
+        double cost = taxi.tripOptions(7.5, 60, 20, 0, 1000, 100, 5, 100);
         System.out.println("Стоимость поездки: " + cost);
-
     }
-
 }
 
---
+++ src/main/java/taxiCalculation.java
@@ -0,0 +1,22 @@
+public class taxiCalculation {
+    double tripOptions(double distance,
+                       int landingAmount,
+                       int costOneKilometer,
+                       double discount,
+                       int minimumDiscountAmount,
+                       int maximumDiscount,
+                       int discountPercentage,
+                       int maximumDiscountPercentage) {
+        double cost = landingAmount + distance * costOneKilometer;
+        if (cost > minimumDiscountAmount) {
+            discount = cost * discountPercentage / maximumDiscountPercentage;
+        }
+        if (discount <= maximumDiscount) {
+            cost -= discount;
+        }
+        if (discount > maximumDiscount) {
+            cost -= maximumDiscount;
+        }
+        return cost;
+    }
+}
